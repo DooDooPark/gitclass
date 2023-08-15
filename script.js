eb03830148cfa2db7a61ea6e77e62c6a6bfe10e1
박두현 <doubled4017@gmail.com>
2023-08-15 14:02:43 +0900

    15일 마지막 수업.

---
+++ script.js
@@ -0,0 +1,24 @@
+
+console.log('hello javascript');
+
+// 변수 선언과 함수 작성
+
+var age = 25;
+let year = 2023;  // 문자, 숫자
+const point = 250;
+const today = '광복절'; // 문자.
+
+const pass = true;
+
+if (pass) {
+    console.log('hello')
+}
+
+// data type.
+// Number = 1,2,3,4,5, 7.5, 8.5 // int, float, 
+// String = 'hello world' 
+// Boolean = true/false / 참,거짓
+// Null, Undefined,Object
+// docker // => 제가 직접 세팅을 해줘야.
+// heroku // => 셋업, 회사가 특정한 셋업 환경이 있습니다.
+// 배포 // heroku docker => 코드를 감싸주구, docker => 배포
